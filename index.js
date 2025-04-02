85012597a6d59128de84f313f83f79e93e97b128
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-02 21:53:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 js
-import { auth, db } from "./firebase.js";  //  Importe Firebase
+// Importer Firebase depuis firebase.js
+import { auth, db } from "./firebase.js";  
 
 console.log(" Firebase est bien connecté !");
 console.log(" Authentification Firebase :", auth);
 console.log(" Base de données Firebase :", db);
-console.log("Bienvenue sur TrendCashX !");
