0b3416579c514f92162e01e97385191f4df63c75
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-02 21:05:23 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,25 @@
+js
+// Importation des modules Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Pour l'authentification
+import { getFirestore } from "firebase/firestore"; // Pour la base de données
+
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AlzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
+  authDomain: "trendcashx.firebaseapp.com",
+  projectId: "trendcashx",
+  storageBucket: "trendcashx.appspot.com",
+  messagingSenderId: "198131315912",
+  appId: "1:198131315912:web :d533275cf1c4849f74a1fd",
+  measurementId: "G-B0SN3CN30Z"
+};
+
+// Initialisation de Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialisation des services Firebase
+const auth = getAuth(app); // Authentification
+const db = getFirestore(app); // Base de données Firestore
+
+export { auth, db };
