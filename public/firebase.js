5588345fafea1f4680d30302affe9272c6dc202f
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-03 18:25:03 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -7,10 +7,10 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebas
 
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "trendcashx.firebaseapp.com",
+  apiKey: "AlzaSyCG1Pn09Ugf0xGknitJTdZCcI70YbhSEk",
+  authDomain: "trendcashx.firebasestorage.com",
   projectId: "trendcashx",
-  storageBucket: "trendcashx.appspot.com",
+  storageBucket: "trendcashx.firebasestorage.app",
   messagingSenderId: "198131315912",
   appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
   measurementId: "G-B0SN3CN30Z"
