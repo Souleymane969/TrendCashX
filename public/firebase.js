7254834b67f7032bdb9ef8b48f2511d08be6c859
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-03 17:42:27 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,11 +1,11 @@
 javascript
 console.log("Firebase.js chargé !");
 
-// Importation des modules Firebase
+//Importation des modules Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"; import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-// Configuration Firebase
+//Configuration Firebase
 const firebaseConfig = {
   apiKey: "AlzaSyCG1Pn09UgfOgxGknitJTdZCcI70YbhSEk",
   authDomain: "trendcashx.firebaseapp.com",
@@ -16,7 +16,7 @@ const firebaseConfig = {
   measurementId: "G-B0SN3CN30Z"
 };
 
-// Initialisation de Firebase
+//Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
