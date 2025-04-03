0698e1cd9bf26675f9b9dc710abe0b62ad79df3f
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-03 15:53:44 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,20 +1,19 @@
-'''javascript
-console.log("Firebase.js
-chargé !");           
-// Importation des modules Firebase depuis une URL correcte
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+javascript
+console.log("Firebase.js chargé !");
+
+// Importation des modules Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"; import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
-    authDomain: "trendcashx.firebaseapp.com",
-    projectId: "trendcashx",
-    storageBucket: "trendcashx.firebasestorage.app",
-    messagingSenderId: "198131315912",
-    appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
-    measurementId: "G-B0SN3CN30Z"
+  apiKey: "AlzaSyCG1Pn09UgfOgxGknitJTdZCcI70YbhSEk",
+  authDomain: "trendcashx.firebaseapp.com",
+  projectId: "trendcashx",
+  storageBucket: "trendcashx.appspot.com",
+  messagingSenderId: "198131315912",
+  appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
+  measurementId: "G-B0SN3CN30Z"
 };
 
 // Initialisation de Firebase
@@ -22,5 +21,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exporter Firebase pour l'utiliser ailleurs
 export { auth, db };
+```
