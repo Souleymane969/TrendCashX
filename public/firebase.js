dfa1196006b382fd8f0e951e47707cf70462b57e
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-03 21:10:37 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,16 +1,16 @@
-'''javascript
-console.log("Firebase.js chargé !");
-// Importation des modules Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+```js
+// Importation des modules Firebase depuis les CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AlzaSyCG1Pn09Ugf0xGknitJTdZCcI70YbhSEk",
+  apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
   authDomain: "trendcashx.firebaseapp.com",
   projectId: "trendcashx",
-  storageBucket: "trendcashx.firebasestorage.app",
+  storageBucket: "trendcashx.appspot.com",  //  Correction ici
   messagingSenderId: "198131315912",
   appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
   measurementId: "G-B0SN3CN30Z"
@@ -18,10 +18,10 @@ const firebaseConfig = {
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-console.log("Firebase chargé avec succès !");
-
-export { auth, db };
+// Rendre Firebase accessible globalement dans le navigateur
+window.firebase = { app, auth, db, analytics }; console.log(" Firebase chargé avec succès !");
 ```
