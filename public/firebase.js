be7f5183c1455ec5ce98d56bf8e1e8ada4c9afc1
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-04 14:35:19 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,17 +1,17 @@
-javascript
-// Importation des modules Firebase depuis les CDN
+// Importation des modules Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js"; import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
-    apiKey: "AlzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
-    authDomain: "trendcashx.firebaseapp.com",
-    projectId: "trendcashx",
-    storageBucket: "trendcashx.appspot.com",
-    messagingSenderId: "198131315912",
-    appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
-    measurementId: "G-B0SN3CN30Z"
+  apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
+  authDomain: "trendcashx.firebaseapp.com",
+  projectId: "trendcashx",
+  storageBucket: "trendcashx.appspot.com",
+  messagingSenderId: "198131315912",
+  appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
+  measurementId: "G-B0SN3CN30Z"
 };
 
 // Initialisation de Firebase
@@ -19,9 +19,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Rendre Firebase accessible dans Eruda
-window.firebaseApp = app;
+// Rendre Firebase accessible globalement
+window.firebase = app;
 window.auth = auth;
 window.db = db;
-
-console.log(" Firebase chargé avec succès !");
