fbec00ab579cc80dce942f4e8e8b0e899f4ae6c7
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-04 09:06:12 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,16 +1,15 @@
-```js
-// Importation des modules Firebase depuis les CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+javascript
+// Importation des modules Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
   authDomain: "trendcashx.firebaseapp.com",
   projectId: "trendcashx",
-  storageBucket: "trendcashx.appspot.com",  //  Correction ici
+  storageBucket: "trendcashx.appspot.com",
   messagingSenderId: "198131315912",
   appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
   measurementId: "G-B0SN3CN30Z"
@@ -18,10 +17,8 @@ const firebaseConfig = {
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Rendre Firebase accessible globalement dans le navigateur
-window.firebase = { app, auth, db, analytics }; console.log(" Firebase chargé avec succès !");
-```
+// Exporter les services Firebase
+export { app, auth, db };
