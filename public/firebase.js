cb2a62a2bf98a27113ee68fea4cdc29a54ccbd37
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-04 22:40:39 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,14 +1,14 @@
 // Importation de Firebase depuis le CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
   authDomain: "trendcashx.firebaseapp.com",
   projectId: "trendcashx",
-  storageBucket: "trendcashx.firebasestorage.app",
+  storageBucket: "trendcashx.appspot.com",
   messagingSenderId: "198131315912",
   appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
   measurementId: "G-B0SN3CN30Z"
@@ -18,8 +18,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-// Rendre Firebase accessible globalement dans la console Eruda
-window.firebase = app;
 
-// Exporter les services Firebase si nécessaire
-export { auth, db };
+// Rendre Firebase accessible dans la console
+window.firebaseApp = app;
+window.firebaseAuth = auth;
+window.firebaseDB = db;
+[04/04, 22:33] ChatGPT: // Tester l'initialisation dans la console
+console.log("Firebase App:", window.firebaseApp);
+console.log("Firebase Auth:", window.firebaseAuth);
+console.log("Firebase Firestore:", window.firebaseDB);
