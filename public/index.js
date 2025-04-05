d343fde6a376760db9393e1b53dfe6fa2afe5f6b
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-05 12:51:53 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -3,31 +3,28 @@ console.log("Script index.js chargé !");
 import { firebaseAuth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-// Sélection du formulaire et du message
 const signupForm = document.getElementById("signup-form");
 const signupMessage = document.getElementById("signup-message");
 
-// Vérifie que le formulaire existe
 if (signupForm) {
   signupForm.addEventListener("submit", async (event) => {
-    event.preventDefault(); // Empêche le rechargement
+    event.preventDefault();
 
     const email = document.getElementById("email").value;
- const password = document.getElementById("password").value;
+    const password = document.getElementById("password").value;
 
     try {
       const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password);
       const user = userCredential.user;
-
-      signupMessage.innerText = `Utilisateur inscrit : user.email`;
+ signupMessage.innerText = ` Utilisateur inscrit : user.email`;
       signupMessage.style.color = "green";
       console.log("Utilisateur inscrit :", user);
      catch (error) 
-      signupMessage.innerText = `Erreur :{error.message}`;
+      signupMessage.innerText = ` Erreur :{error.message}`;
       signupMessage.style.color = "red";
       console.error("Erreur d'inscription :", error);
     }
   });
 } else {
-  console.error("Le formulaire d'inscription est introuvable !");
-}
+  console.error("Le formulaire d'inscription n'existe pas.");
+} 
