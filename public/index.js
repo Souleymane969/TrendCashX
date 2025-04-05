bf71b3e6a2c8b1b0e20ebb13f6b889a9f9ce27c6
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-05 21:10:43 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -9,7 +9,30 @@ const signupMessage = document.getElementById("signup-message");
 if (signupForm) {
   signupForm.addEventListener("submit", async (event) => {
     event.preventDefault();
+// -------- Connexion --------
+const loginForm = document.getElementById("login-form");
+const loginMessage = document.getElementById("login-message");
 
+if (loginForm) {
+  loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
+      const user = userCredential.user;
+      loginMessage.innerText = `Bienvenue user.email `;
+      loginMessage.style.color = "green";
+      console.log("Utilisateur connecté :", user);
+     catch (error) 
+      loginMessage.innerText = `Erreur de connexion :{error.message}`;
+ loginMessage.style.color = "red";
+      console.error("Erreur de connexion :", error);
+    }
+  });
+}
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
