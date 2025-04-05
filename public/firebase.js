8ccc9488791cb141dcab49c32b790e5a79d11d6b
Souleymane969 <sanogosouleymane5@gmail.com>
2025-04-05 11:41:39 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,29 +1,25 @@
-// Importation de Firebase depuis le CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+// Importation des fonctions depuis le CDN Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
+  apiKey: "AlzaSyCG1Pn09Ugf0gxGknitJTdZCcI70YbhSEk",
   authDomain: "trendcashx.firebaseapp.com",
   projectId: "trendcashx",
-  storageBucket: "trendcashx.appspot.com", //  Correction ici (avant c'était firebasestorage.app)
+  storageBucket: "trendcashx.appspot.com",
   messagingSenderId: "198131315912",
   appId: "1:198131315912:web:d533275cf1c4849f74a1fd",
   measurementId: "G-B0SN3CN30Z"
 };
 
-// Initialisation de Firebase
+// Initialisation de l'app
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-//  Rendre Firebase accessible dans la console pour le débogage
-window.firebaseApp = app;
-window.firebaseAuth = auth;
-window.firebaseDB = db;
-//  Vérifier que Firebase est bien chargé
-console.log(" Firebase App:", window.firebaseApp);
-console.log(" Firebase Auth:", window.firebaseAuth);
-console.log(" Firebase Firestore:", window.firebaseDB);
+// Initialisation des services
+const firebaseAuth = getAuth(app);
+const firebaseDB = getFirestore(app);
+
+// Exporter les variables nécessaires
+ export { firebaseAuth, firebaseDB };
